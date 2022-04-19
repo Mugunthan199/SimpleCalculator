3cf3d41537fe66d83446a56b3eaa6b3594b3aaaa
unknown <mugunthanganesh78@gmail.com>
2022-04-20 00:12:41 +0530

    newLineDelimiterTest and newLineAfterCommaTest

--- src/main/java/com/mugunthco/basicCalcy/Add.java
+++ src/main/java/com/mugunthco/basicCalcy/Add.java
@@ -2,20 +2,26 @@ package com.mugunthco.basicCalcy;
 
 import java.util.stream.Stream;
 
+/**
+ * This class is used for String addition basic calculator
+ * @author Uma Ganesan
+ *
+ */
 public class Add {
 	
+	/**
+	 * This method returns addition of n number of values passed as String
+	 * @param numbers
+	 * @return
+	 */
 	public static int add(String numbers)
 	{
-		if(numbers.equals("")||numbers == null)
+		if(!numbers.isEmpty())
 		{
-			return 0;
-		}
-		else
-		{
-		String num[] = numbers.split(",");
+		String num[] = numbers.split(",|\n");
 		int sum = Stream.of(num).mapToInt(Integer::parseInt).sum();
 		return sum;
 		}
+		return 0;
 	}
-
 }
--- src/test/java/com/mugunthco/basicCalculatorTests/BasicCalculatorTest.java
+++ src/test/java/com/mugunthco/basicCalculatorTests/BasicCalculatorTest.java
@@ -1,5 +1,7 @@
 package com.mugunthco.basicCalculatorTests;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -12,11 +14,32 @@ class BasicCalculatorTest {
 		Assertions.assertEquals(Add.add(""),0);
 	}
 	@Test
+	void OneNumberInputTest() {
+		Assertions.assertEquals(Add.add("7"),7);
+	}
+	@Test
 	void basicNumbersWithoutDelimitersTest() {
 		Assertions.assertEquals(Add.add("1,4"),5);
 	}
 	@Test
 	void unknownAmountOfNumbersAddTest() {
 		Assertions.assertEquals(Add.add("1,4,5,7"),17);
+	}
+	@Test
+	void basicTwoNumbersWithNewLineDelimiterTest() {
+		Assertions.assertEquals(Add.add("1,4,5\n7"),17);
+	}
+	@Test
+	void basicTwoNumbersWithNewLineDelimiterAfterCommaTest() {
+		boolean thrown = false;
+		try
+		{
+		Assertions.assertEquals(Add.add("1,4,5,\n7"),17);
+		}
+		catch(NumberFormatException e)
+		{
+			thrown = true;
+		}
+		assertTrue(thrown);
 	}
 }
